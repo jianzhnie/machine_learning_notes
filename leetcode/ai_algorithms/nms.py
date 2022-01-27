cdcdd33ede50d88915f48e9f31e6a2527702cda6
jianzhnie <jianzhnie@126.com>
2022-01-27 12:16:09 +0800

    Create nms.py

---
+++ leetcode/ai_algorithms/nms.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def NMS(dets, thresh):
+
+    # x1、y1、x2、y2、以及score赋值
+    # （x1、y1）（x2、y2）为box的左上和右下角标
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+
+    # 每一个候选框的面积
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    # order是按照score降序排序的，得到的是排序的本来的索引，不是排完序的原数组
+    order = scores.argsort()[::-1]
+    # ::-1表示逆序
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        # 计算当前概率最大矩形框与其他矩形框的相交框的坐标
+        # 由于numpy的broadcast机制，得到的是向量
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.minimum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.maximum(y2[i], y2[order[1:]])
+
+        # 计算相交框的面积,注意矩形框不相交时w或h算出来会是负数，需要用0代替
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        # 计算重叠度IoU
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        # 找到重叠度不高于阈值的矩形框索引
+        inds = np.where(ovr <= thresh)[0]
+        # 将order序列更新，由于前面得到的矩形框索引要比矩形框在原order序列中的索引小1，所以要把这个1加回来
+        order = order[inds + 1]
+    return keep
+
+
+if __name__ == '__main__':
+    dets = np.array([[310, 30, 420, 5, 0.6], [20, 20, 240, 210, 1],
+                     [70, 50, 260, 220, 0.8], [400, 280, 560, 360, 0.7]])
+    # 设置阈值
+    thresh = 0.4
+    keep_dets = NMS(dets, thresh)
+    # 打印留下的框的索引
+    print(keep_dets)
+    # 打印留下的框的信息
+    print(dets[keep_dets])
