c59deb203d78f5a9080626764517b4f687f98081
jianzhnie <jianzhnie@126.com>
2022-01-28 22:08:39 +0800

    Update generateMatrix.py

--- leetcode/generateMatrix.py
+++ leetcode/generateMatrix.py
@@ -3,7 +3,7 @@ from typing import List
 import numpy as np
 
 
-class Solution:
+class Solution_:
     def generateMatrix(self, n: int) -> List[List[int]]:
         # 初始化要填充的正方形
         matrix = [[0] * n for _ in range(n)]
@@ -46,8 +46,48 @@ class Solution:
         return matrix
 
 
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m - 1
+        up, down = 0, n - 1
+        res = []
+        while left < right and up < down:
+
+            for i in range(left, right):
+                number = matrix[up][i]
+                res.append(number)
+
+            for j in range(up, down):
+                number = matrix[j][right]
+                res.append(number)
+
+            for i in range(right, left, -1):
+                number = matrix[down][i]
+                res.append(number)
+
+            for j in range(down, up, -1):
+                number = matrix[j][left]
+                res.append(number)
+
+            left += 1
+            right -= 1
+            up += 1
+            down -= 1
+
+        if n % 2:
+            number = matrix[n // 2][n // 2]
+            res.append(number)
+        return res
+
+
 if __name__ == '__main__':
+    solution = Solution_()
+    ans = solution.generateMatrix(10)
+    ans = np.array(ans)
+    print(ans)
+
     solution = Solution()
-    ans = solution.generateMatrix(6)
+    ans = solution.spiralOrder(ans)
     ans = np.array(ans)
     print(ans)
