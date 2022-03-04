c09f45535562b936b66155708d402ede521b8ad2
jianzhnie <jianzhnie@126.com>
2022-03-04 12:14:15 +0800

    Update 0300_最长上升子序列_.py

--- leetcode/code/0300_最长上升子序列_.py
+++ leetcode/code/0300_最长上升子序列_.py
@@ -11,3 +11,22 @@ def lengthOfLIS(nums: List[int]) -> int:
             if nums[i] > nums[j]:
                 dp[i] = max(dp[i], dp[j] + 1)
     return max(dp)
+
+
+def lengthOfLIS2(nums: List[int]) -> int:
+    d = []
+    for n in nums:
+        if not d or n > d[-1]:
+            d.append(n)
+        else:
+            left, right = 0, len(d) - 1
+            loc = right
+            while left <= right:
+                mid = (left + right) // 2
+                if d[mid] >= n:
+                    loc = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            d[loc] = n
+    return len(d)
